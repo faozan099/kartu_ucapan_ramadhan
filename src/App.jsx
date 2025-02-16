0dd9e347a23c241ca0c3b02b53ca29aaeeda5417
faozan099 <125945029+faozan099@users.noreply.github.com>
2025-02-16 12:36:28 +0800

    perbaruan

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import Desktop from './components/Desktop'
 import Mobile from './components/Mobile'
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.addEventListener('resize', handleResize)
+
+  }, [])
 
   return (
     <>
-    <Mobile />
+      {isMobile ? <Mobile /> : <Desktop />}
     </>
   )
 }
