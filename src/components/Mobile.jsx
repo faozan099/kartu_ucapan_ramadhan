d3592b72748366870bdf74ab16d643d9b524007f
faozan099 <125945029+faozan099@users.noreply.github.com>
2025-02-25 10:05:54 +0800

    90%

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,20 +1,44 @@
+import gsap from "gsap"
 import logo from "../assets/logo.png"
 import imgspin from "../assets/imgspin.png"
-import test from "../assets/test.png"
+import ornamen from "../assets/ornamen.png"
+import { useEffect, useRef } from "react"
+import { TextPlugin } from "gsap/all"
+import Aos from "aos"
+import "aos/dist/aos.css"
 import "./mobile.css"
 
+
+gsap.registerPlugin(TextPlugin)
+
 function Mobile() {
+    const textRef = useRef(null)
+    useEffect(()=> {
+        gsap.to(textRef.current, {
+            duration: 2,
+            text: "PT SOVAA KARYA INDONESIA",
+            ease: "none"
+        }
+    )
+    }, [])
+
+    useEffect(()=> {
+        Aos.init({duration: 1000, once: true});
+        Aos.refresh()
+    }, [])
+    
     return (
         <div className='frame'>
             <div className='container'>
                 <div className='containerFlex'>
-                    <img src={logo} alt="" className="logo"/>
-                    <p className="deskripsi">Selamat menunaikan ibadah puasa Ramadhan 1446 H</p>
-                    <hr className="hr1"/>
+                    <img src={logo} alt="" className="logo" data-aos="fade-down" />
+                    <p className="deskripsi" data-aos="fade-down" >Selamat menunaikan ibadah puasa Ramadhan 1446 H</p>
+                    <hr className="hr1" />
                     <hr className="hr2" />
-                    <p className="inc">PT SOVAA KARYA INDONESIA</p>
-                    <img src={test} className="test" alt="" />
-                    <img src={imgspin} alt="" className="imgspin"/>
+                    <p className="inc" ref={textRef}></p>
+                    <img src={ornamen} className="ornamen" alt="" />
+                    <img src={imgspin} alt="" className="imgspin" />
+                    <img src={imgspin} alt="" className="imgspin2" />
                 </div>
             </div>
         </div>
